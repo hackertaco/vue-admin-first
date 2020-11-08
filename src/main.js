43cb04edf433c8c29dd76655bfa0f657f6d3a5ea
hackertaco <imsa1224@naver.com>
2020-11-08 16:49:28 +0900

    feat; ⚡️setting

--- src/main.js
+++ src/main.js
@@ -1,4 +1,19 @@
 import { createApp } from 'vue'
+// import { sync } from 'vuex-router-sync'
+// import './components'
+// import './plugins'
+
 import App from './App.vue'
+import router from '@/router'
+// import store from '@/store'
+// import theme from './plugins/theme'
+// import 'vuetify/dist/vuetify.min.css'
+// import Vuetify from 'vuetify'
+import '@mdi/font/css/materialdesignicons.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+// sync(router,store)
+const app = createApp(App);
+app.use(router);
+app.config.productionTip = false
 
 createApp(App).mount('#app')
